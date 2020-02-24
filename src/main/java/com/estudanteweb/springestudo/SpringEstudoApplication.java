4921fb2fcb531ec027e11170d1bfbeb9a0d15d33
Cicero Jonyedson <cicerojonyedison@gmail.com>
2020-02-24 13:40:56 -0300

    Project created

---
+++ src/main/java/com/estudanteweb/springestudo/SpringEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.estudanteweb.springestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEstudoApplication.class, args);
+	}
+
+}
