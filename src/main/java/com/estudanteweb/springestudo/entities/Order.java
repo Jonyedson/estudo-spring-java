9597adfa463bc5a99876e2c89f3cf439ca9bdeac
Cicero Jonyedson <cicerojonyedison@gmail.com>
2020-02-26 13:42:58 -0300

    Order, Instant, ISO 8601  - PART 2

--- src/main/java/com/estudanteweb/springestudo/entities/Order.java
+++ src/main/java/com/estudanteweb/springestudo/entities/Order.java
@@ -2,7 +2,6 @@ package com.estudanteweb.springestudo.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
@@ -20,7 +21,10 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -29,8 +33,10 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Date moment, User client) {
-
+	public Order(Long id, Instant moment, User client) {
+		this.id = id;
+		this.moment = moment;
+		this.client = client;
 	}
 
 	public Long getId() {
---
+++ src/main/java/com/estudanteweb/springestudo/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.estudanteweb.springestudo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudanteweb.springestudo.entities.Order;
+import com.estudanteweb.springestudo.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
