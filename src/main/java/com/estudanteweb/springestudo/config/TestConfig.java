932ef1fb3b2e7a9c6fb4b5917c882bed747ed550
Cicero Jonyedson <cicerojonyedison@gmail.com>
2020-02-27 00:06:58 -0300

    Payment, one-to-one association

--- src/main/java/com/estudanteweb/springestudo/config/TestConfig.java
+++ src/main/java/com/estudanteweb/springestudo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudanteweb.springestudo.entities.Category;
 import com.estudanteweb.springestudo.entities.Order;
 import com.estudanteweb.springestudo.entities.OrderItem;
+import com.estudanteweb.springestudo.entities.Payment;
 import com.estudanteweb.springestudo.entities.Product;
 import com.estudanteweb.springestudo.entities.User;
 import com.estudanteweb.springestudo.entities.enums.OrderStatus;
@@ -76,6 +77,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 
 }
